4e359efd0348e7be14bd529aac1b1d7295f7481a
Imran Sayed <imranhsayed@gmail.com>
2019-03-28 16:16:58 +0530

    Add identity-obj-proxy to mock css and images

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  collectCoverageFrom: ["src/**/*.{js,jsx,mjs}"],
-  testMatch: ["<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}", "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}"],
-  transform: {
-	"^.+\\.(js|jsx|mjs)$": "<rootDir>/config/jest-transformer.js"
-  },
-  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$"]
+	"moduleNameMapper": {
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+		"\\.(css|less)$": "identity-obj-proxy"
+	}
 };
--- src/components/ItemList/index.js
+++ src/components/ItemList/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import styles from '../../Home.css';
 
 const ItemList = ( { items } ) => {
 	console.log( 'items', items );
