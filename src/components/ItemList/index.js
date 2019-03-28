c5fdedec4063ea688d2cdadd2c1b034acb469d4c
Imran Sayed <imranhsayed@gmail.com>
2019-03-28 16:49:03 +0530

    Add --coverage flag to the jest script

--- src/components/ItemList/index.js
+++ src/components/ItemList/index.js
@@ -3,6 +3,9 @@ import styles from '../../Home.css';
 
 const ItemList = ( { items } ) => {
 	console.log( 'items', items );
+	localStorage.setItem( 'myname', 'Imran' );
+	console.log( localStorage.getItem( 'myname' ) );
+
 	return items.length ? (
 		<ul>{items.map(i => <li key={i}>{i}</li>)}</ul>
 	) : (
